d8ff78a9e1cec1f272a0ddcd0c1249507f896bff
Karla Sequen <karlasequen@gmail.com>
2022-09-01 18:16:21 -0600

    add graph

---
+++ graph.js
@@ -0,0 +1,37 @@
+class Graph {
+    constructor() {
+        this.nodes = 0;
+        this.adjecentList = {};
+    }
+
+    addVertex(node) {
+        this.adjecentList[node] = [];
+        this.nodes++;
+    }
+
+    addEdge(node1, node2) {
+        this.adjecentList[node1].push(node2);
+        this.adjecentList[node2].push(node1);
+    }
+}
+
+const myGraph = new Graph();
+
+myGraph.addVertex("1");
+myGraph.addVertex("3");
+myGraph.addVertex("4");
+myGraph.addVertex("5");
+myGraph.addVertex("6");
+myGraph.addVertex("8");
+
+console.log(myGraph);
+
+myGraph.addEdge("3", "5");
+myGraph.addEdge("6", "3");
+myGraph.addEdge("1", "6");
+myGraph.addEdge("1", "3");
+myGraph.addEdge("1", "4");
+myGraph.addEdge("4", "5");
+myGraph.addEdge("8", "4");
+
+console.log(myGraph);
